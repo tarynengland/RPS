a56be0a096de7a0d0600d15e780e7b015ffca7ca
Taryn England <englandtt@hendrix.edu>
2022-04-24 18:16:57 -0500

    Making a Java file.

---
+++ src/RPS/Objects.java
@@ -0,0 +1,22 @@
+package RPS;
+
+import javafx.scene.paint.Color;
+
+public enum Objects {
+    ROCK {
+        public Color getColor() {
+            return Color.BROWN;
+        }
+    },
+    PAPER{
+        public Color getColor(){
+            return Color.WHITE;
+        }
+    },
+    SCISSORS {
+        public Color getColor(){
+            return Color.RED;
+        }
+    };
+    public abstract Color getColor();
+}
---
+++ src/RPS/Position.java
@@ -0,0 +1,4 @@
+package RPS;
+
+public class Position {
+}
