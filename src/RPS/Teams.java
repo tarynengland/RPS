ae54b025767aea231331f6a00b13caec1191f058
Taryn England <englandtt@hendrix.edu>
2022-05-02 10:25:47 -0500

    working on teams class

--- src/RPS/Teams.java
+++ src/RPS/Teams.java
@@ -1,4 +1,20 @@
 package RPS;
 
+import javafx.geometry.Bounds;
+
+import java.util.ArrayList;
+
 public class Teams {
+
+    private ArrayList<Player> team;
+    private static int members;
+    private Objects role;
+
+    public Teams(int members, Bounds arena){
+        team = new ArrayList<Player>();
+        for (int i = 0; i <= members; i++){
+            Player player = new Player(role, arena);
+            team.add(player);
+        }
+    }
 }
